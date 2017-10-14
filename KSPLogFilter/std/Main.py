ee3d557b640e6a72416f90c7b147e10391f1af40
icecubeinanicecube <benefranke@t-online.de>
2017-10-14 12:06:14 +0200

    keywords now work, help available

--- KSPLogFilter/std/Main.py
+++ KSPLogFilter/std/Main.py
@@ -13,14 +13,19 @@ def nichtFiltern(x, keyword):
         return warning and keyword in x
 
 defaultLogPath = "C:/Steam/steamapps/common/Kerbal Space Program/KSP.log"
-defaultOutPath = "C:/ELF/errors.txt"
+defaultOutPath = "errors.txt"
 keyword = ""
+about = "Usage: ELF [-o <path>] [-s <path>] [-k <keyword>] \n-o: specifies the output path \n-s: specifies the path of the source log\n-k: specifies a keyword to search for, case-sensitive. Can be used to filter for only ERRORS by using -k ERR (WARNING: beta-feature))"
 
 logPath = defaultLogPath
 outPath = defaultOutPath
 
 #evaluate arguments, if none keep defaults
-for i in range(len(argv)-1):
+
+if(argv[1] == "--help"):
+    print(about)
+    exit()
+for i in range(1, len(argv)):   #argv[0] just returns the name of the script
     if(argv[i] == "-o"):
         outPath = argv[i+1]
     elif(argv[i] == "-s"):
